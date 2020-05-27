65a72f1a0fcf52482479ba03641ef957103f7d2b
Vsevolod Reshetnikov <vsevolod@insurancemenu.com>
2020-05-27 12:23:41 +0300

    Initial commit for maven project

--- src/main/java/rsa/Main.java
+++ src/main/java/rsa/Main.java
@@ -1,9 +1,9 @@
 package rsa;
 
-import java.io.IOException;
-import java.math.BigInteger;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
